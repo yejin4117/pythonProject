c2f3dc76ef0f8420b8a69b02ec53d2d3609c999a
이 예진 <s2108@e-mirim.hs.kr>
2022-07-05 09:04:03 +0900

    제곱 list

---
+++ python2_2210 이예진.py
@@ -0,0 +1,88 @@
+# #2210 이예진
+# #1
+# def sum_odd(result):
+#
+# result = sum_odd()
+# if result % 2 == 1:
+#
+#
+# result = sum_odd('01012345678')
+# print(result)
+# result = sum_odd('01022224444')
+# print(result)
+# result = sum_odd('01099999999')
+# print(result)
+#
+#
+#
+#
+# #2
+# def encrypt():
+#     return
+# for n in :
+#
+# if n == 'a' or n == 'e' or n == 'i' or n == 'o' or n == 'u':
+#     continue
+#     print(n, end='*')
+#
+# print(encrypt('ive'))
+# print(encrypt('nct dream'))
+# print(encrypt('AEiou'))
+# print(encrypt('GOOGLE'))
+# print(encrypt('BTS'))
+
+#3
+# def dec_to_bin():
+#     return
+#
+#
+#
+# print(dec_to_bin(10))
+# print(dec_to_bin(2))
+# print(dec_to_bin(77))
+# print(dec_to_bin(1024))
+#
+#
+# # #4
+# # def abbreviate():
+# # #5
+# #6
+
+def get_bmi(height, weight):
+    height = 0
+    weight = 0
+
+    bmi = height * 2 // weight
+
+    if bmi < 18.5:
+        print("저체중")
+    elif 18.5 <= bmi < 23:
+        print("정상")
+    elif 23 <= bmi < 25:
+        print("과체중")
+    elif 25 <= bmi:
+        print("비만")
+
+    return height,weight
+
+print(get_bmi(height = 170, weight=60))
+print(get_bmi(height = 150, weight=60))
+print(get_bmi(height = 180, weight=50))
+print(get_bmi(height = 160, weight=60))
+
+
+
+#7
+def minus_time():
+
+    return
+
+sign, hour, minute = minus_time(hour1=13,minue=40, hour2=6,minute2=33)
+print(sign,hour,minute)
+
+#8
+def rgb_to_hex():
+    return
+
+
+#2210 이예진
